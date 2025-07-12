4e619416e8430419c83a34ded1edc6f8e68974a5
Nicolas AMATI <skkyz3r.dev@gmail.com>
2025-07-12 23:08:35 +0200

    init tailwindcss and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/ui',
-    '@nuxt/image'
-  ]
-})
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/ui", "@nuxt/image"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
